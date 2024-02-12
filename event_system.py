48d86b01e2ae6e0a7ac50cf9388b4ba999fe0c6d
joelhalen <joel@joelhalen.net>
2024-02-12 14:39:36 -0800

    Load and store game state in memory on startup Update memory on change

--- event_system.py
+++ event_system.py
@@ -11,6 +11,8 @@
 ###                                                                                            ###
 ###     Please maintain the above copyright notice in redistributions or modifications.        ###
 ##################################################################################################
+import asyncio
+
 from utils.db.database import Database
 
 
@@ -18,8 +20,33 @@ database = Database()
 
 
 class EventManager:
-    def __init__(self):
+    def __init__(self, database):
         self.database = database
+        self.games = {}  # Key: game_id, Value: game details
+        self.players = {}  # Key: player_id, Value: player details
+        self.teams = {}  # Key: team_id, Value: team details
+
+        asyncio.run(self.load_data_from_db())
+
+    async def load_data_from_db(self):
+        await self.load_games()
+        await self.load_players()
+        await self.load_teams()
+
+    async def load_games(self):
+        games = await self.database.fetch_data("SELECT * FROM games")
+        for game in games:
+            self.games[game['game_id']] = game
+
+    async def load_players(self):
+        players = await self.database.fetch_data("SELECT * FROM players")
+        for player in players:
+            self.players[player['player_id']] = player
+
+    async def load_teams(self):
+        teams = await self.database.fetch_data("SELECT * FROM teams")
+        for team in teams:
+            self.teams[team['team_id']] = team
 
     async def create_game(self, game_id, game_params):
         game_exists = await self.database.fetch_data("SELECT game_id FROM games WHERE game_id = %s", (game_id,))
@@ -29,6 +56,7 @@ class EventManager:
             "INSERT INTO games (game_id, game_params, state) VALUES (%s, %s, 'waiting')",
             (game_id, game_params)
         )
+        self.games[game_id] = {'game_id': game_id, 'game_params': game_params, 'state': 'waiting'}
         return f"Game {game_id} created"
 
     async def register_player(self, game_id, player_id, team_id=None):
@@ -47,6 +75,7 @@ class EventManager:
                 "INSERT INTO players (player_id, game_id) VALUES (%s, %s)",
                 (player_id, game_id)
             )
+        self.players[player_id] = {'player_id': player_id, 'game_id': game_id, 'team_id': team_id}
         return f"Player {player_id} registered to game {game_id}"
 
     async def create_team(self, team_id, game_id):
@@ -57,6 +86,7 @@ class EventManager:
             "INSERT INTO teams (team_id, game_id) VALUES (%s, %s)",
             (team_id, game_id)
         )
+        self.teams[team_id] = {'team_id': team_id, 'game_id': game_id}
         return f"Team {team_id} created for game {game_id}"
 
     async def assign_player_to_team(self, player_id, team_id):
